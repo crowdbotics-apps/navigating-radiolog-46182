64908ff1094e177853736ec897f25a72d0463cae
Crowdbotics <team@crowdbotics.com>
2024-02-09 15:53:30 +0000

    Initial commit

---
+++ backend/navigating_radiolog_46182/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for navigating_radiolog_46182 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "navigating_radiolog_46182.settings"
+)
+
+application = get_wsgi_application()
